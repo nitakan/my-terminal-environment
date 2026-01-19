a744c9372b7ea3b18dd7ee33c72070fcc4b57b62
nitakan <masanori.kato@kyash.co>
2026-01-19 10:29:41 +0900

    chore(zsh): 不要なコメント化されたAnyenv設定とDenoのPATH設定を削除

--- zsh/modules/exports.zsh
+++ zsh/modules/exports.zsh
@@ -2,16 +2,6 @@ export PATH="/opt/homebrew/bin:$PATH"
 export PATH="$PATH:$HOME/tools"
 export PATH="/usr/local/bin:$PATH"
 
-# Anyenv
-# export PATH="$HOME/.anyenv/bin:$PATH"
-# eval "$(/opt/homebrew/bin/brew shellenv)"
-# eval "$(anyenv init -  --no-rehash)"
-# eval "$(rbenv init -  --no-rehash)"
-
-# Deno
-
-export PATH="$HOME/.deno/bin:$PATH"
-
 # Android
 export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
 
